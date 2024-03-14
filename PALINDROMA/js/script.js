d137bef95259f654fe0fdb93184985f228cb79c3
simone-danzeri <s.danzeri@icloud.com>
2024-03-14 15:22:41 +0100

    feat: add start of function

--- PALINDROMA/js/script.js
+++ PALINDROMA/js/script.js
@@ -5,7 +5,7 @@
 // Chiedo all'utente un parola
 const userWord = prompt('Ciao! Dimmi una parola qualsiasi');
 console.log(userWord);
-// Dovrò leggermi la parola in stile array 
+// Dovrò leggermi la parola in stile array
 for (let i = 0; i < userWord.length; i++) { // NON CAPISCO PERCHE MA IN CONSOLE LE LETTERE DOPPIE ME LE ACCORPA DICENDO CHE C'E' UNA RIPETIZIONE
     let eachUserWordLetter = userWord[i];
     console.log(eachUserWordLetter);
@@ -15,3 +15,8 @@ for (let i = 0; i < userWord.length; i++) { // NON CAPISCO PERCHE MA IN CONSOLE 
 // FUNCTIONS
 
 // Controlla se la parola che inserisco è palindroma
+// word --> stringa che dovrò controllare se palindroma
+// return: stringa in cui dichiaro se word è palindroma oppure no
+function isPalindrome (word) {
+    // Devo capire se la sequenza delle lettere inserite dall'utente è uguale al suo reverso
+}
