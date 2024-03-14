12bbc2146bee4d013691e6f698c68cec9de80069
simone-danzeri <s.danzeri@icloud.com>
2024-03-14 14:49:56 +0100

    feat: PALINDROMA - add userWord

--- PALINDROMA/js/script.js
+++ PALINDROMA/js/script.js
@@ -1,3 +1,17 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+// Chiedo all'utente un parola
+const userWord = prompt('Ciao! Dimmi una parola qualsiasi');
+console.log(userWord);
+// Dovrò leggermi la parola in stile array 
+for (let i = 0; i < userWord.length; i++) { // NON CAPISCO PERCHE MA IN CONSOLE LE LETTERE DOPPIE ME LE ACCORPA DICENDO CHE C'E' UNA RIPETIZIONE
+    let eachUserWordLetter = userWord[i];
+    console.log(eachUserWordLetter);
+}
+
+
+// FUNCTIONS
+
+// Controlla se la parola che inserisco è palindroma
